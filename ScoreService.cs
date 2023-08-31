8fd7991785a1809b6399d73af8221b59300613aa
Kwinu <137926873+Kwinu@users.noreply.github.com>
2023-08-31 13:50:24 +0200

    Add files via upload

---
+++ ScoreService.cs
@@ -0,0 +1,44 @@
+﻿using SharpDX.Direct3D9;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursDLC
+
+    public interface IServiceScore
+{
+    void Add(int pPoints);
+    int Get();
+}
+
+{
+    internal class ScoreService : IServiceScore
+    {
+        private int Value;
+
+        public ScoreService()
+        {
+            Value = 0;
+            ServiceLocator.RegisterService<IServiceScore>(this);
+        }
+
+        public void Add(int pPoints)
+        {
+            Value += pPoints;
+        }
+
+        public int Get()
+        {
+            return Value;
+        }
+
+        public void Display()
+        {
+            Debug.WriteLine(Value);
+        }
+    }
+}
---
+++ ServiceLocator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursDLC
+{
+    public static class ServiceLocator
+    {
+        private static readonly Dictionary<Type, object> listServives = new Dictionary<Type, object>();
+
+        public static void RegisterService<T>(T service)
+        {
+            listServives[typeof(T)] = service;
+        }
+
+        public static T GetService<T>()
+        {
+            return (T)listServives[typeof(T)];
+        }
+
+    }
+}
